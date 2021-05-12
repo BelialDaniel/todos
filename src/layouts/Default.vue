c03cf8606dc0d3b55495fc1c8155c4816a6e26ed
Daniel Maldonado <belialdaniel@gmail.com>
2021-05-11 22:01:59 -0500

    Removed vue components

---
+++ src/layouts/Default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main">
+    <slot>main slot</slot>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  padding: 0;
+  margin: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <Default>
+    <div>home</div>
+  </Default>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Default from "@/layouts/Default.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Default,
   },
 })
 export default class Home extends Vue {}
