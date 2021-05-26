5c9d442bd27db17fc5d81701b164aa1ff9ac406c
Daniel Maldonado <belialdaniel@gmail.com>
2021-05-25 22:47:20 -0500

    Added authentication check in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,21 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 import Home from "../views/Home.vue"
+import { auth } from "../firebase"
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
   },
   {
     path: "/about",
@@ -15,12 +25,24 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      requiresAuth: true,
+    },
   },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth)
+  if (requiresAuth && !auth.currentUser) {
+    next("/login")
+  } else {
+    next()
+  }
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <Default>
+    <div>Login</div>
+  </Default>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component"
+import Default from "@/layouts/Default.vue"
+
+@Options({
+  components: {
+    Default,
+  },
+})
+export default class Login extends Vue {}
+</script>
